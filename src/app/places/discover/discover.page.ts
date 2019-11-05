a841d08e7d4a8e6caaac8d2a7eea39a7294687dc
pfsuarez <pfsuarez@gmail.com>
2019-11-05 08:33:37 -0300

    added places to discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,3 +1,5 @@
+import { Place } from './../place.model';
+import { PlacesService } from './../places.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  loadedPlaces: Place[];
 
-  constructor() { }
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.placesService.getPlaces();
   }
 
 }
