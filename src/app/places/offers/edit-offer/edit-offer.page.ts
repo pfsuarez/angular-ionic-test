3047efcacb7860316e3cb3d79aacb4ade77ca8bb
pfsuarez <pfsuarez@gmail.com>
2019-11-18 17:49:39 -0300

    added Reactive Forms to edit-offer page

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
 import { Place } from './../../place.model';
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
+  form: FormGroup;
   place: Place;
 
   constructor(
@@ -27,7 +29,26 @@ export class EditOfferPage implements OnInit {
       }
 
       this.place = this.placesService.getPlace(paramMap.get('placeId'));
+
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
     });
+  }
+
+  onUpdateOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    console.log(this.form);
   }
 
 }
