752fdf3a6e51b1c227dadc0bde355b1fd18304b0
pfsuarez <pfsuarez@gmail.com>
2019-12-04 08:33:02 -0300

    Getting a formatted address from lat&lng

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { ModalController } from '@ionic/angular';
 
 import { MapModalComponent } from './../../map-modal/map-modal.component';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-location-picker',
@@ -12,7 +15,8 @@ import { MapModalComponent } from './../../map-modal/map-modal.component';
 export class LocationPickerComponent implements OnInit {
 
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private http: HttpClient
   ) { }
 
   ngOnInit() { }
@@ -23,8 +27,31 @@ export class LocationPickerComponent implements OnInit {
     }).then(modalEl => {
       modalEl.onDidDismiss().then(modalData => {
         console.log('', modalData.data);
+        if (!modalData.data) {
+          return;
+        }
+
+        this.getAddress(modalData.data.lat, modalData.data.lng)
+          .subscribe(address => {
+            console.log(address);
+          });
       });
       modalEl.present();
     });
+  }
+
+  private getAddress(lat: number, lng: number) {
+
+    return this.http.get<any>(`${environment.googleGeocodingUrl}&latlng=${lat},${lng}`)
+      .pipe(
+        map(geoData => {
+          console.log('', geoData);
+          if (!geoData || !geoData.results || geoData.results === 0) {
+            return null;
+          }
+
+          return geoData.results[0].formatted_address;
+        })
+      );
   }
 }
