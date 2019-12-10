8ed52311ba18427346d5e4b56018f69751ee3f52
pfsuarez <pfsuarez@gmail.com>
2019-12-10 08:53:19 -0300

    implement camera plugin

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
+import { ImagePickerComponent } from './pickers/image-picker/image-picker.component';
 import { MapModalComponent } from './map-modal/map-modal.component';
 
 @NgModule({
-  declarations: [LocationPickerComponent, MapModalComponent],
-  exports: [LocationPickerComponent, MapModalComponent],
+  declarations: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
+  exports: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
   entryComponents: [MapModalComponent],
   imports: [
     CommonModule,
